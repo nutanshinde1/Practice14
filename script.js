a9ff0047788f623392e4cd5c79e2c18a8b854baa
Nutan Arjun Shinde <shindenutan3714@gmail.com>
2025-09-04 20:58:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('searchBtn').addEventListener('click', () => {
  
     fetch(url)  
       .then(response => response.json())   
-      .then(data => {
+      .then(data => { 
         if (data.cod === 200) {
           const temp = (data.main.temp - 273.15).toFixed(2);
           const weather = data.weather[0].description;
