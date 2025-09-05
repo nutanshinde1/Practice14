8ee275089005a62c9cd2f5fdc01cd2c5742497e4
Nutan Arjun Shinde <shindenutan3714@gmail.com>
2025-09-05 23:14:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('searchBtn').addEventListener('click', () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   
  
-    fetch(url)  
+    fetch(url)   
       .then(response => response.json())   
       .then(data => { 
         if (data.cod === 200) {
